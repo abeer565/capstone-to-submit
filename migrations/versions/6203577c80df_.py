264a933a0f4aa2dd42b5ea1b243f0453cc64cbdf
abeer <53477589+abeer565@users.noreply.github.com>
2021-09-18 16:21:07 +0300

    Add files via upload

---
+++ migrations/versions/6203577c80df_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: 6203577c80df
+Revises: 
+Create Date: 2021-09-18 16:19:25.132590
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '6203577c80df'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
